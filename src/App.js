28382ac0398e200ff34887a1cc5b7e8082f00b46
James <jjohnston899@yahoo.co.uk>
2021-02-03 14:24:43 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
 
 function App() {
+//ref is similar to state allows you to store a value but will not cause you to re-render like state would 
+  const [name, setName] = useState('');
+  const inputRef = useRef();
+
+  //store the previous value of the name 
+  const prevName = useRef('');
+
+//useEffect to allow the previously entered name to be saved 
+//this will run when the name changes
+//ref is good for persisting the elements across renders
+
+useEffect(() => {
+prevName.current = name;
+
+}, [name]);
+
+  //function which will run and give focus to the input when button is clicked
+
+  //will give the focus of the user to the text box when the button is clicked 
+  function focus(){
+    inputRef.current.focus();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
+   <div>My name is {name} and it used to be {prevName.current}</div>
+   <button onClick={focus}>Focus</button>
+   </>
   );
 }
 
